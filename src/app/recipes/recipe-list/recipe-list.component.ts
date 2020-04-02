95fee397fd61119c2f546d01393f91741acd5b03
Nicodemus <nicodemus.maurits@gmail.com>
2020-04-02 18:33:26 +0200

    Display recipe dynamically

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       'A test recipe',
       'This is simply a test',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
     )
   ];
   constructor() {}
