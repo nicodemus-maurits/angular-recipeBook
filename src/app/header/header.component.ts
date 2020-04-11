f3afc75d20e219e7943d44499f1a46a43c2429de
Nicodemus <nicodemus.maurits@gmail.com>
2020-04-11 12:47:07 +0200

    Changed header implementation regarding with authentication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authenticatedUserSubscription: Subscription;
+  isAuthenticated: boolean = false;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authenticatedUserSubscription = this.authService.authenticatedUser.subscribe(
+      (user) => {
+        // Will return true if there is a user available
+        this.isAuthenticated = !!user;
+      }
+    );
+  }
 
   onStoreData() {
     this.dataStorageService.storeRecipes();
@@ -15,5 +32,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.authenticatedUserSubscription.unsubscribe();
   }
 }
